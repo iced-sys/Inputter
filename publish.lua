68f8f359322029ae7c6c595ef79944741529d78a
Adam Mills <adamm1404@outlook.com>
2025-04-28 13:54:24 +0100

    Added build-rbxm script

--- publish.lua
+++
@@ -1,7 +0,0 @@
-local fs = require("@lune/fs")
-local roblox = require("@lune/roblox")
- 
-local placeFile = fs.readFile("place.rbxl")
-local game = roblox.deserializePlace(placeFile)
-
-print("Publishing to Roblox...")
